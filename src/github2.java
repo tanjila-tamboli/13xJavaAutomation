053f8429e8d2ac9d5d2af0ced3f0b674f9181f1a
tanjila-tamboli <tanjila.tamboli704@gmail.com>
2025-06-06 19:49:25 +0530

    github2

--- src/github2.java
+++ src/github2.java
@@ -3,5 +3,9 @@ public class github2
     public static void main (String args[])
     {
         System.out.println("github2");
+
+        System.out.println("github2");
+        System.out.println("github2");
+        System.out.println("github2");
     }
 }
