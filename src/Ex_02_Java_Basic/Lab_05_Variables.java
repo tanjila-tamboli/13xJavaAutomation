be61cbb9b63a37422cb77cb51743d49e42e04ba0
tanjila-tamboli <tanjila.tamboli704@gmail.com>
2025-06-12 23:18:02 +0530

    Variables

---
+++ src/Ex_02_Java_Basic/Lab_05_Variables.java
@@ -0,0 +1,20 @@
+package Ex_02_Java_Basic;
+
+public class Lab_05_Variables {
+    public static void main(String[] args) {
+
+
+        public static void main(int args){
+            /*This is an overloaded version of main.
+              It will not run automatically when the program starts.
+              It will only run if you call it manually from the original main method*/
+        }
+            public static void main()
+            {
+                /*Another overloaded method with no parameters.
+                  Again, JVM won’t call this automatically, but you can call it manually*/
+
+            }
+        }
+    }
+}
---
+++ src/Ex_02_Java_Basic/Lab_06_Variables.java
@@ -0,0 +1,25 @@
+package Ex_02_Java_Basic;
+
+public class Lab_06_Variables {
+    public static void main(String[] args) {
+
+                // ```
+                //data_type variable_name =  variable_value
+                //```
+
+                byte b = 10;
+                // byte -> data type - type of container -
+                // store only integer value but limited to max and min
+                // -128 to 127
+
+                // b ->  varibale name -> identifier
+
+                // = -> assignment - assign the value to the b
+
+                // 10 -> literal -> integer value
+
+
+            }
+        }
+
+
