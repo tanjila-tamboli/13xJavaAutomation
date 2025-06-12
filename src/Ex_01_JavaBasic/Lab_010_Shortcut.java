1a662796c05dad040e6f1b2fe4f68b2f81e8c73c
tanjila-tamboli <tanjila.tamboli704@gmail.com>
2025-06-12 11:54:23 +0530

    Shortcut

---
+++ src/Ex_01_JavaBasic/Lab_010_Shortcut.java
@@ -0,0 +1,11 @@
+package Ex_01_JavaBasic;
+
+import com.sun.tools.javac.Main;
+
+public class Lab_010_Shortcut {
+    public static void main (String args[])
+    {
+        System.out.println("hello world");
+
+    }
+}
