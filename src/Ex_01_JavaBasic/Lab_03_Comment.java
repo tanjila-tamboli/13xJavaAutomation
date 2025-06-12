d07554cb341ff5b33a23c809a2ae04991508dbfb
tanjila-tamboli <tanjila.tamboli704@gmail.com>
2025-06-12 19:17:42 +0530

    Single and Multi Line Comment Shortcut

---
+++ src/Ex_01_JavaBasic/Lab_03_Comment.java
@@ -0,0 +1,12 @@
+package Ex_01_JavaBasic;
+
+public class Lab_03_Comment {
+    public static void main (String Arg[])
+    {
+        //this is code for comments
+      /*  System.out.println(" Tanjila");
+        System.out.println(" Tamboli");
+        System.out.println(" Tanjila");
+        System.out.println(" Tamboli");*/
+    }
+}
