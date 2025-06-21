1e762e5a23d039bd6c52d37d620db7e49858ab12
tanjila-tamboli <tanjila.tamboli704@gmail.com>
2025-06-21 17:37:36 +0530

    Operators

---
+++ src/Ex_09_Operators/Lab_36_Assignment_operator.java
@@ -0,0 +1,12 @@
+package Ex_09_Operators;
+
+public class Lab_36_Assignment_operator {
+    public static void main(String args[])
+    {
+        int a= 65;
+        // int is datatype
+        //a is identifier
+        //= is operator
+        // 65 is literal
+    }
+}
---
+++ src/Ex_09_Operators/Lab_37_Arithmetic_operators.java
@@ -0,0 +1,19 @@
+package Ex_09_Operators;
+
+public class Lab_37_Arithmetic_operators {
+    public static void main(String args[])
+    {
+        int a=5;
+        float b=3.14f;
+        int c = 85;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(a-c);
+        System.out.println(a*c);
+
+        System.out.println("a+b");
+    }
+
+}
